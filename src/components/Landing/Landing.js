83ddf93276afaf46754fa9632f97a5df3d21f54c
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-28 14:09:34 -0500

    Add content to Landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -5,8 +5,8 @@ const Landing = () => {
   return (
     <>
       <Header />
-      <div></div>
-      <button></button>
+      <div className="pitch"><p>but you still need a signature drink for company happy hour</p></div>
+      <button className='enter-button'>browse mocktails</button>
     </>
   )
 }
