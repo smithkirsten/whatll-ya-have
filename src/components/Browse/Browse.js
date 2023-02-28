06d2c8ab73844f5eed3f665306baec99f6848c85
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-28 16:46:12 -0500

    Add useState to Browse

--- src/components/Browse/Browse.js
+++ src/components/Browse/Browse.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 import './Browse.css'
+import Header from "../Header/Header";
 
 const Browse = () => {
+  const [drinks, setDrinks] = useState([])
 
+  const createCards = () => {
+    
+  }
+  return (
+    <>
+      <Header page='browse' />
+      <section className="card-display">
+        {createCards()}
+      </section>
+    </>
+  )
 }
 
 export default Browse
