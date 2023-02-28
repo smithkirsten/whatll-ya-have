d1f7670a6f32328bac03b375c0778b6496eb8ce8
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-28 14:15:42 -0500

    Add header component logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 const Header = () => {
+  return (
+    <nav className="header">
+      <img className="logo" src="/assets/neonlogo.png" alt="what'll ya have logo" />
+    </nav>
+  )
 
 }
 
