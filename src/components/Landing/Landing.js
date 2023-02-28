26c8f9a835781e454f14bade3fcafcec53550b79
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-28 14:02:42 -0500

    Add basic JSX for landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -2,7 +2,13 @@ import React from "react";
 import Header from "../Header/Header";
 
 const Landing = () => {
-
+  return (
+    <>
+      <Header />
+      <div></div>
+      <button></button>
+    </>
+  )
 }
 
 export default Landing
