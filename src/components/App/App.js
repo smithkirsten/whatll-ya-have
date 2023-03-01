f9b6908f702a0dbd7a68f1c38e9b6ffb27df6c7d
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-03-01 10:19:58 -0500

    Add routes and imports

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router'
 import './App.css';
 import Landing from '../Landing/Landing';
 import Browse from '../Browse/Browse';
+import DrinkDeets from '../DrinkDeets/DrinkDeets';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Landing />}/>
         <Route path='/browse' element={<Browse />}/>
+        <Route path='/drink/:id' element={<DrinkDeets />} />
       </Routes>
     </main>
   );
