a2831f70fce2ae832186d26def944683cfa1ed6b
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-03-01 10:39:40 -0500

    Add state to DrinkDeets

--- src/components/DrinkDeets/DrinkDeets.js
+++ src/components/DrinkDeets/DrinkDeets.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
 const DrinkDeets = () => {
+  const [drink, setDrink] = useState()
 
+  return (
+    <>
+
+    </>
+  )
 }
 
 export default DrinkDeets
