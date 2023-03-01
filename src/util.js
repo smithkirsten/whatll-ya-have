603565a6db6dab11c14c55b6b176ea1d778c2b6f
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-03-01 17:15:53 -0500

    Add logic to cleaning function

--- src/util.js
+++ src/util.js
@@ -1,3 +1,21 @@
-export const cleanDrink = () => {
+export const cleanDrink = (drink) => {
+  const keys = Object.keys(drink)
+  const keepers = ['idDrink', 'strDrink', 'strTags', 'strGlass', 'strInstructions', 'strDrinkThumb']
+  keys.forEach(key => {
+    if (!keepers.includes(key) || (key.includes('strIngredient') && !drink[key]) || (key.includes('strMeasure') && !drink[key])) {
+      delete drink[key]
+    }
+    console.log(drink)
+    return drink
+  })
 
 }
+
+//idDrink
+//strDrink
+//strTags
+//strGlass
+//strInstructions
+//strDrinkThumb
+//strIngredient[]
+//strMeasure[]
