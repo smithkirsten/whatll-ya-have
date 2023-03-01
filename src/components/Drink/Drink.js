a80d38fd9aa9ee0c7367340681b505758210df43
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-03-01 09:25:51 -0500

    Size drink cards and set dynamic backgrounds

--- src/components/Drink/Drink.js
+++ src/components/Drink/Drink.js
@@ -1,12 +1,19 @@
 import React from "react";
+import './Drink.css'
 import { NavLink } from "react-router-dom";
 
 const Drink = ({ drink }) => {
+  const background = { 
+    backgroundImage: `url(${drink.strDrinkThumb})`,  
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover'
+  }
   return (
     <NavLink>
       <article className="drink-card" id={drink.idDrink}>
-        <div className='drink-image'>
-          <p className='drink-name'>{drink.strDrink}</p>
+        <div className='drink-image' style={background}>
+          <p className='drink-name hidden'>{drink.strDrink}</p>
         </div>
       </article>
     </NavLink>
