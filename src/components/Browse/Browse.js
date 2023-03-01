43f1a76e449a54ee9d6e61fc4f5e7f55fc6a65d4
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-03-01 08:53:31 -0500

    Add mock data to useEffect to avoid overcharging the API

--- src/components/Browse/Browse.js
+++ src/components/Browse/Browse.js
@@ -12,17 +12,43 @@ const Browse = () => {
   const [loading, setLoading] = useState('')
 
   useEffect(() => {
-    if(!drinks) {
-      fetchData()
-        .then(data => {
-          console.log(data)
-          setDrinks(data.drinks)
-        })
-        .catch((error) => {
-          console.log(error)
-          setError(error)
-        })
-    }
+    // if(!drinks) {
+    //   fetchData()
+    //     .then(data => {
+    //       console.log(data)
+    //       setDrinks(data.drinks)
+    //     })
+    //     .catch((error) => {
+    //       console.log(error)
+    //       setError(error)
+    //     })
+    // }
+    setDrinks([{
+      "strDrink": "Afterglow",
+      "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/vuquyv1468876052.jpg",
+      "idDrink": "12560"
+  },
+  {
+    "strDrink": "Alice Cocktail",
+    "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/qyqtpv1468876144.jpg",
+    "idDrink": "12562"
+  },
+  {
+    "strDrink": "Aloha Fruit punch",
+    "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/wsyvrt1468876267.jpg",
+    "idDrink": "12862"
+  },
+  {
+    "strDrink": "Apello",
+    "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/uptxtv1468876415.jpg",
+    "idDrink": "15106"
+  },
+  {
+  "strDrink": "Apple Karate",
+  "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/syusvw1468876634.jpg",
+  "idDrink": "12564"
+  }
+])
   }, [])
 
   const createCards = () => {
