16b17e9d1bad21cea67054b379d85d545d690f40
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-03-01 15:12:16 -0500

    Add state, params, and fetch to DrinkDeets

--- src/components/DrinkDeets/DrinkDeets.js
+++ src/components/DrinkDeets/DrinkDeets.js
@@ -1,13 +1,34 @@
 import React from "react";
+import { fetchData } from "../../apiCalls";
 import { useState, useEffect } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
+import Header from "../Header/Header";
 
-const DrinkDeets = () => {
-  const [drink, setDrink] = useState()
+const DrinkDeets = (props) => {
+  const [drink, setDrink] = useState({})
+  const [error, setError] = useState('')
+  const { id } = useParams()
+
+  useEffect(() => {
+
+    fetchData(id)
+      .then(data => {
+        console.log(data)
+        setDrink(data.drinks)
+      })
+      .catch(error => setError(error))
+  }, [])
 
   return (
     <>
-
+      <Header page='drink'/>
+      <div className="fav-bar">
+        <p>IS THIS YOUR DRINK?</p>
+        <img />
+      </div>
+      <img />
+      <div className="ingredients"></div>
+      <div className="directions"></div>
     </>
   )
 }
