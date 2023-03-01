6a0787aee1f8fdc0c036dbdb5305aa30e626b61a
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-03-01 08:49:23 -0500

    Add conditional to fetch call and move data assignment to useEffect

--- src/components/Browse/Browse.js
+++ src/components/Browse/Browse.js
@@ -2,23 +2,39 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import './Browse.css'
+import { fetchData } from "../../apiCalls";
 import Header from "../Header/Header";
 import Drink from '../Drink/Drink'
 
 const Browse = () => {
   const [drinks, setDrinks] = useState([])
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState('')
 
   useEffect(() => {
-    setDrinks()
+    if(!drinks) {
+      fetchData()
+        .then(data => {
+          console.log(data)
+          setDrinks(data.drinks)
+        })
+        .catch((error) => {
+          console.log(error)
+          setError(error)
+        })
+    }
   }, [])
 
   const createCards = () => {
     return drinks.map(drink => <Drink />)
   }
+
   return (
     <>
       <Header page='browse' />
       <section className="card-display">
+        {error && <p>error city</p>}
+        {!drinks && <p>loading...</p>}
         {drinks && createCards()}
       </section>
     </>
