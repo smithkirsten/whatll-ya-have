3faf9e2d2eec561e29dd29627d8c1a5ec501b6d3
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-03-01 08:48:36 -0500

    Change fetch call to route through RapidAPI

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,12 +1,28 @@
-export const fetchData = async(id) => {
-  const url = id ? `www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}` : `www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic`
-  try {
-    const response = await fetch(url)
-    const data = await response.json()
-    return data
-  } catch (error) {
-    console.log(error)
-    return error
-  }
-  
+export const fetchData = () => {
+  // try {
+  //   const response = await fetch(url)
+  //   const data = await response.json()
+  //   console.log(data)
+  //   return data
+  // } catch (error) {
+  //   console.log(error)
+  //   return error
+  // }
+  // console.log('fetching')
+  // return fetch("www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic")
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '0ba55045bemsh7c87450d2a771b8p155de8jsn591dafeb3f86',
+      'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com'
+    }
+  };
+  return fetch('https://the-cocktail-db.p.rapidapi.com/filter.php?a=Non_Alcoholic', options)
+          .then((response) => {
+            if(response.ok) {
+              return response.json()
+            } else {
+              throw new Error('Whoops, something went wrong')
+            }
+          })
 }
