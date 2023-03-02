d3b6b35d0934ca8ea4cac61dd09dc024d9df62d6
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-03-02 10:18:41 -0500

    Change logical rendering

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import './Header.css'
 
 const Header = ({ page }) => {
 
+const content = <p>there are so many reasons to kick alcohol to the curb</p>
+const favButton = <NavLink to='/favorites'><button className="nav-buttons">FAVORITES</button></NavLink>
+const browseButton = <NavLink to='/browse'><button className='nav-buttons'></button>
+</NavLink>
+
   return (
     <nav className="header">
       <img className="logo" src="/assets/neonlogo.png" alt="what'll ya have logo" />
-      {page === 'landing' && <p className="reason">there are so many reasons to kick alcohol to the curb</p>}
-      {page === 'browse' || page === 'drink' && <button>favorites</button>}
-      {page === 'favorites' || page === 'drink' && <button>browse all</button>}
+      {page === 'landing' ? content : <div className='buttons' style={}>{buttons}</div>}
     </nav>
   )
 
