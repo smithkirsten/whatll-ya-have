d647ffc0a128dad548ece0de5f78af54c0953a58
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-03-02 11:23:09 -0500

    Conditionally render buttons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,13 +6,32 @@ const Header = ({ page }) => {
 
 const content = <p>there are so many reasons to kick alcohol to the curb</p>
 const favButton = <NavLink to='/favorites'><button className="nav-buttons">FAVORITES</button></NavLink>
-const browseButton = <NavLink to='/browse'><button className='nav-buttons'></button>
-</NavLink>
+const browseButton = <NavLink to='/browse'><button className='nav-buttons'>BROWSE ALL</button></NavLink>
+let buttons;
+let position;
+const style = {
+  display: 'flex',
+  justifyContent: position,
+}
+
+const determineButtons = () => {
+  if (page === 'browse') {
+    position = 'flex-end'
+    return favButton
+  }else if(page === 'favorites') {
+    position = 'flex-start'
+    return browseButton
+  } else {
+    position = 'space-between'
+    return [favButton, browseButton]
+  }
+}
+
 
   return (
     <nav className="header">
       <img className="logo" src="/assets/neonlogo.png" alt="what'll ya have logo" />
-      {page === 'landing' ? content : <div className='buttons' style={}>{buttons}</div>}
+      {page !== 'landing' && <div className='buttons' style={style}>{buttons}</div>}
     </nav>
   )
 
