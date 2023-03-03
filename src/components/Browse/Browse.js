31db9cbbf1773ac8c9e6f5b50c524c5f23bed663
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-03-03 10:03:59 -0500

    Remove state from Browse

--- src/components/Browse/Browse.js
+++ src/components/Browse/Browse.js
@@ -1,70 +1,14 @@
 import React from "react";
-import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import './Browse.css'
-import { fetchData } from "../../apiCalls";
-import { addFavProp } from "../../util";
 import Header from "../Header/Header";
 import Drink from '../Drink/Drink'
 
-const dummyArray = [{
-  "strDrink": "Afterglow",
-  "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/vuquyv1468876052.jpg",
-  "idDrink": "12560"
-},
-{
-"strDrink": "Alice Cocktail",
-"strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/qyqtpv1468876144.jpg",
-"idDrink": "12562"
-},
-{
-"strDrink": "Aloha Fruit punch",
-"strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/wsyvrt1468876267.jpg",
-"idDrink": "12862"
-},
-{
-"strDrink": "Apello",
-"strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/uptxtv1468876415.jpg",
-"idDrink": "15106"
-},
-{
-"strDrink": "Apple Karate",
-"strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/syusvw1468876634.jpg",
-"idDrink": "12564"
-}
-]
 
 const Browse = () => {
-  const [drinks, setDrinks] = useState([])
-  const [error, setError] = useState('')
-  const [loading, setLoading] = useState('')
   const path = window.location.href.split('/')[3]
 
-  useEffect(() => {
-    // if(!drinks.length) {
-    //   fetchData()
-    //     .then(data => {
-    //       console.log(data)
-    //       setDrinks(data.drinks)
-    //     })
-    //     .catch((error) => {
-    //       console.log(error)
-    //       setError(error)
-    //     })
-    // }
-    if(!drinks.length) {
-      setDrinks(addFavProp(dummyArray))
-    }
-  }, [])
-  const toggleFav = (id) => {
-    const updatedDrinks = drinks.map(drink => {
-      if(drink.idDrink === id) {
-        drink.favorite = !drink.favorite
-      }
-      return drink
-    })
-    setDrinks(updatedDrinks)
-  }
+
 
   const createCards = (favorites) => {
     const showDrinks = path === 'favorites' ? drinks.filter(drink => drink.favorite): drinks;
