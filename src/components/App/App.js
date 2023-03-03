4d6d033057ed95d9d32e948b634a09be729fc979
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-03-03 10:03:33 -0500

    Move state to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,82 @@
+// import React from "react";
 import { Route, Routes } from 'react-router'
+import { useState, useEffect } from "react";
+import { fetchData } from "../../apiCalls";
+import { addFavProp } from "../../util";
 import './App.css';
 import Landing from '../Landing/Landing';
 import Browse from '../Browse/Browse';
 import DrinkDeets from '../DrinkDeets/DrinkDeets';
 
+const dummyArray = [{
+  "strDrink": "Afterglow",
+  "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/vuquyv1468876052.jpg",
+  "idDrink": "12560"
+},
+{
+"strDrink": "Alice Cocktail",
+"strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/qyqtpv1468876144.jpg",
+"idDrink": "12562"
+},
+{
+"strDrink": "Aloha Fruit punch",
+"strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/wsyvrt1468876267.jpg",
+"idDrink": "12862"
+},
+{
+"strDrink": "Apello",
+"strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/uptxtv1468876415.jpg",
+"idDrink": "15106"
+},
+{
+"strDrink": "Apple Karate",
+"strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/syusvw1468876634.jpg",
+"idDrink": "12564"
+}
+]
+
 function App() {
+  const [drinks, setDrinks] = useState([])
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    // if(!drinks.length) {
+    //   fetchData()
+    //     .then(data => {
+    //       console.log(data)
+    //       setDrinks(data.drinks)
+    //     })
+    //     .catch((error) => {
+    //       console.log(error)
+    //       setError(error)
+    //     })
+    // }
+    if(!drinks.length) {
+      setDrinks(addFavProp(dummyArray))
+    }
+})
+
+    const toggleFav = (id) => {
+      const updatedDrinks = drinks.map(drink => {
+        if(drink.idDrink === id) {
+          drink.favorite = !drink.favorite
+        }
+        return drink
+      })
+      setDrinks(updatedDrinks)
+    }
+
   return (
     <main>
       <Routes>
-        <Route path='/browse' element={<Browse />}/>
-        <Route path='/favorites' element={<Browse />} />
-        <Route path='/drink/:id/:favorite' element={<DrinkDeets />} />
+        <Route path='/browse' element={<Browse drinks={drinks} toggleFav={toggleFav}/>} />
+        <Route path='/favorites' element={<Browse drinks={drinks} toggleFav={toggleFav} />} />
+        <Route path='/drink/:id/:favorite' element={<DrinkDeets drinks={drinks} toggleFav={toggleFav} />} />
         <Route path='/' element={<Landing />}/>
       </Routes>
     </main>
   );
 }
+
 
 export default App;
