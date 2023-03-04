6604bbc149dc2ac1ba1a66676d7afdec43f8d02f
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-03-04 10:10:33 -0500

    Add intercepts to browse and landing

--- cypress/e2e/landing.cy.js
+++ cypress/e2e/landing.cy.js
@@ -1,5 +1,14 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Landing Page', () => {
+  beforeEach(() => {
+    cy.intercept(
+      "GET",
+      'https://the-cocktail-db.p.rapidapi.com/filter.php?a=Non_Alcoholic',
+      {
+        fixture: '../fixtures/allDrinks'
+      }
+    )
+  })
+  it('has the logo', () => {
     cy.visit('https://example.cypress.io')
   })
 })
