554915ec76e5db3dd415c69f0c8c7ea5bdb6be9d
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-03-04 13:49:56 -0500

    Add it blocks to drink test

--- cypress/e2e/drink.cy.js
+++ cypress/e2e/drink.cy.js
@@ -1,5 +1,44 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('DrinkDeets', () => {
+  beforeEach(() => {
+    cy.intercept(
+      "GET",
+      "https://the-cocktail-db.p.rapidapi.com/lookup.php?i=15106",
+      {
+        fixture: '../fixtures/allDrinks'
+      }
+    )
+    cy.visit("http://localhost:3000/drink/15106");
+  })
+
+  it('has the logo', () => {
+    cy.get('img[class="logo"]').should('be.visible')
+  })
+
+  it('has a favorites button in the header that navs to the favorites page', () => {
+    cy.get('div[class="buttons"]').should('be.visible')
+      cy.get('#favButton').click()
+      cy.url().should("eq", "http://localhost:3000/favorites");
+  })
+
+  it('has a browse all button in the header that navs to the browse page', () => {
+    cy.get('div[class="buttons"]').should('be.visible')
+      cy.get('#browseAll').click()
+      cy.url().should("eq", "http://localhost:3000/browse");
+  })
+
+  it('should have an image of the drink', () => {
+   
+  })
+  it('should have the drink name', () => {
+   
+  })
+  it('should have an image of the drink', () => {
+   
+  })
+  it('should have a clickable favorite button', () => {
+   
+  })
+  it('should have instructions', () => {
+   
   })
 })
