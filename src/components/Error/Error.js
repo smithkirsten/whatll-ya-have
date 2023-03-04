600e711add9f8518b93c93d4a46fda592619cd74
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-03-04 15:45:19 -0500

    Add error return

---
+++ src/components/Error/Error.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './Landing.css'
+import Header from "../Header/Header";
+
+const Landing = ({ drinks }) => {
+  return (
+    <>
+      <Header page='error' />
+      <section className="error-page">
+        <div className="pitch"><img src='/assets/broken-glass.png' alt='broken glass' /><p>Whoops! Something went wrong</p></div>
+      </section>
+    </>
+  )
+}
+
+export default Landing
